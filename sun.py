edee9d35e0bf411a6e6a8b0cf439eee3553c4e10
Yarik-Popov <yarikpopov03@gmail.com>
2023-03-25 17:08:08 -0400

    Nearly done script
    Need to change type_print and header file contents

--- sun.py
+++ sun.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import argparse
 import json
+import os
 import struct
 import sys
 from requests import Response
@@ -17,18 +18,8 @@ DATA_DOUBLE = 'd'
 DEFAULT_STEP_SIZE = '1m'
 DEFAULT_TARGET = 'sun'
 DEFAULT_FILE_OUTPUT = 'output.bin'
+DEFAULT_EXCLUDE = 'last'
 
-# Script variables used to
-start_time = '2030-01-01'
-stop_time = '2030-01-02'
-ephem_type = 'APPROACH'
-output_format = 'json'
-target = 'sun'
-step_size = '1m'
-url = f'https://ssd.jpl.nasa.gov/api/horizons.api?format={output_format}&MAKE_EPHEM=YES&EPHEM_TYPE=VECTORS&COMMAND=' \
-      f'{target}&OBJ_DATA=NO&STEP_SIZE={step_size}&START_TIME={start_time}&STOP_TIME={stop_time}&CSV_FORMAT=YES&' \
-      f'CAL_FORMAT=JD&VEC_TABLE=1'
-file_output = 'output.bin'
 type_print = ON_WRITE_PRINT
 
 
@@ -37,15 +28,26 @@ def define_parser() -> argparse.ArgumentParser:
     Defines the parser for the script
     :return: The parser
     """
-
-    parser = argparse.ArgumentParser(description='Sun position calculator')
-    parser.add_argument('start-time', type=str, help='Start time in the format YYYY-MM-DD')
-    parser.add_argument('stop-time', type=str, help='Stop time in the format YYYY-MM-DD')
-    parser.add_argument('-s', '--step-size', type=str, default=DEFAULT_STEP_SIZE)
-    parser.add_argument('-t', '--target', type=str, default=DEFAULT_TARGET)
-    parser.add_argument('-o', '--output', type=str, default=DEFAULT_FILE_OUTPUT)
-    parser.add_argument('-d', '--double', action='store_true', default=False)
-    parser.add_argument('-p', '--print', type=int, choices=range(3), default=ALWAYS_PRINT)
+    parser = argparse.ArgumentParser(description='Position Ephemeris Retriever')
+    parser.add_argument('start_time', type=str, help='Start time in the format YYYY-MM-DD or JD')
+    parser.add_argument('stop_time', type=str, help='Stop time in the format YYYY-MM-DD or JD')
+    parser.add_argument('-s', '--step-size', type=str, default=DEFAULT_STEP_SIZE,
+                        help=f'Step size in the same format as the horizontal API (e.g. 1m, 1h, 1d, 1y, JD100). '
+                             f'Default: {DEFAULT_STEP_SIZE}')
+    parser.add_argument('-t', '--target', type=str, default=DEFAULT_TARGET,
+                        help=f'Target object (e.g. sun, moon, mars). Default: {DEFAULT_TARGET}')
+    parser.add_argument('-o', '--output', type=str, default=DEFAULT_FILE_OUTPUT,
+                        help=f'Output file name. Default: {DEFAULT_FILE_OUTPUT}')
+    parser.add_argument('-d', '--double', action='store_true', default=False,
+                        help='Use double precision (8 bytes). Default: False (4 bytes) or float')
+    parser.add_argument('-p', '--print', type=int, choices=range(3), default=ALWAYS_PRINT,
+                        help=f'Prints the output to the console. 0 = Always, 1 = On write, 2 = Verbose. '
+                             f'Default: {ALWAYS_PRINT}')
+    # Change to store_false if you want to override the output file by default
+    parser.add_argument('-w', '--write', action='store_true', help='Overrides the output file')
+    parser.add_argument('-e', '--exclude', choices=['first', 'last', 'both', 'none'], default=DEFAULT_EXCLUDE,
+                        help=f'Exclude the first, last, both or none of the values from the output file. '
+                             f'Default: {DEFAULT_EXCLUDE}')
 
     return parser
 
@@ -66,22 +68,25 @@ def print_output_if_required(*values, output_type=ALWAYS_PRINT, sep: str | None 
         print(*values, sep=sep, end=end, file=file, flush=flush)
 
 
-def write_data(data: str, type_data: str):
+def write_data(data: str, is_double: bool, file_output: str):
     """
     Write the parameter data to the output.bin file and check if the written data is within the error bounds
 
-    :param type_data: The type of data to be written ('d' or 'f')
+    :param file_output: The output file
     :param data: Data to be read and written check
-    :return: None or ValueError is raised
+    :param is_double: If True then the data is written as a double precision (8 bytes)
+    otherwise it is written as a float
     """
-    assert type_data == DATA_DOUBLE or type_data == DATA_FLOAT, "Incorrect type"
     expected_data = float(data)
 
     # Appends the data to the file and prints the expected data written if applicable
-    with open(file_output, "ab") as file:
+    with open(file_output, 'ab') as file:
         print_output_if_required(f'\tData written: {expected_data}', output_type=VERBOSE_PRINT)
-        b = struct.pack(type_data, expected_data)
-        byte = bytearray(b)
+        if is_double:
+            b = struct.pack(DATA_DOUBLE, expected_data)
+        else:
+            b = struct.pack(DATA_FLOAT, expected_data)
+        byte: bytearray = bytearray(b)
         file.write(byte)
 
 
@@ -92,7 +97,6 @@ def check_version(data: dict):
     """
     if (data.get('signature')).get('version') != SUPPORTED_VERSION:
         print_output_if_required('WARNING: UNSUPPORTED HORIZON API VERSION USED')
-        input('Press enter to continue: ')
 
 
 def validate_response(response: Response):
@@ -123,11 +127,11 @@ def print_header(reverse=False):
     """
     if reverse:
         print_output_if_required('-' * 130, output_type=ON_WRITE_PRINT)
-        print_output_if_required('\t' * 4, 'JD:', '\t' * 4, 'X:', '\t' * 5, 'Y:', '\t' * 4, 'Z:',
+        print_output_if_required('\t' * 3, 'JD:', '\t' * 4, 'X:', '\t' * 5, 'Y:', '\t' * 4, 'Z:',
                                  output_type=ON_WRITE_PRINT)
         print_output_if_required('-' * 130, output_type=ON_WRITE_PRINT)
     else:
-        print_output_if_required('\t' * 4, 'JD:', '\t' * 4, 'X:', '\t' * 5, 'Y:', '\t' * 4, 'Z:',
+        print_output_if_required('\t' * 3, 'JD:', '\t' * 4, 'X:', '\t' * 5, 'Y:', '\t' * 4, 'Z:',
                                  output_type=ON_WRITE_PRINT)
         print_output_if_required('-' * 130, output_type=ON_WRITE_PRINT)
 
@@ -135,12 +139,17 @@ def print_header(reverse=False):
 def main():
     # TODO: Add argument parsing
     # Submit the API request and decode the JSON-response:
-    type_data = 'f'
+    args = define_parser().parse_args()
+    url = f'https://ssd.jpl.nasa.gov/api/horizons.api?format=json&MAKE_EPHEM=YES&EPHEM_TYPE=VECTORS&COMMAND=' \
+          f'{args.target}&OBJ_DATA=NO&STEP_SIZE={args.step_size}&START_TIME={args.start_time}&STOP_TIME=' \
+          f'{args.stop_time}&CSV_FORMAT=YES&CAL_FORMAT=JD&VEC_TABLE=1'
+
     response = requests.get(url)
     validate_response(response)
 
     try:
         data = json.loads(response.text)
+        print(data)
     except ValueError:
         print("Unable to decode JSON results")
         raise ValueError
@@ -149,7 +158,22 @@ def main():
     check_version(data)
     start = False
     count = 0
+    total_count = 0
+    lines_written = 0
     print_header()
+
+    # Find total number of lines to be written
+    for i in lines:
+        if i.startswith('$$SOE'):
+            start = True
+        if i.startswith('$$EOE'):
+            start = False
+        if start and not i.startswith('$$SOE'):
+            total_count += 1
+
+    # Overwrite the output file if it exists
+    if args.write and os.path.exists(args.output):
+        os.remove(args.output)
 
     # Loop over response
     for i in lines:
@@ -158,24 +182,21 @@ def main():
         if i.startswith('$$EOE'):
             start = False
         if start and not i.startswith('$$SOE'):
-            print_output_if_required(f'Line being parsed: {i}', output_type=VERBOSE_PRINT)
-            output = (i[:-1].split(', '))
-            output.pop(1)
+            if not ((count == 0 and (args.exclude == 'both' or args.exclude == 'first'))
+                    or (count == total_count - 1) and (args.exclude == 'both' or args.exclude == 'last')):
+                print_output_if_required(f'Line being parsed: {i}', output_type=VERBOSE_PRINT)
+                output = (i[:-1].split(', '))
+                output.pop(1)
 
-            print_output_if_required(f'Output written: ', *output, output_type=ON_WRITE_PRINT)
-            for j in output:
-                write_data(j, type_data)
+                print_output_if_required(f'Output written: ', *output, output_type=ON_WRITE_PRINT)
+                for j in output:
+                    write_data(j, args.double, args.output)
+                lines_written += 1
             count += 1
 
     print_header(True)
-    print_output_if_required(f'Lines written: {count}')
-
-
-def test():
-    args = define_parser().parse_args()
-    print(args)
+    print_output_if_required(f'Lines written: {lines_written}')
 
 
 if __name__ == '__main__':
     main()
-    # test()
